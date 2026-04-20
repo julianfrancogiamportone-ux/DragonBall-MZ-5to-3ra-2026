126380e7eb9dcaaa813e238a659db750a0bb3145
unknown <lesliequintanamz@gmail.com>
2026-04-20 10:02:20 -0300

    Nombres bien

---
+++ src/main/java/dragonball/entidades/Peleador.java
@@ -0,0 +1,92 @@
+package dragonball.entidades;
+import jakarta.persistence.*;
+import java.util.List;
+@Entity 
+public class Peleador {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private int puntosVida;
+    private int energia;
+    private float defensaBase;
+    private Arma armaEquipada;
+    private List inventario;
+    private List habilidades;
+
+    @ManyToMany 
+    private List<Peleador> integrantesColaboradores;
+
+    public Peleador() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntosVida() {
+        return puntosVida;
+    }
+
+    public void setPuntosVida(int puntosVida) {
+        this.puntosVida = puntosVida;
+    }
+
+    public int getEnergia() {
+        return energia;
+    }
+
+    public void setEnergia(int energia) {
+        this.energia = energia;
+    }
+
+    public float getDefensaBase() {
+        return defensaBase;
+    }
+
+    public void setDefensaBase(float defensaBase) {
+        this.defensaBase = defensaBase;
+    }
+
+    public Arma getArmaEquipada() {
+        return armaEquipada;
+    }
+
+    public void setArmaEquipada(Arma armaEquipada) {
+        this.armaEquipada = armaEquipada;
+    }
+
+    public List getInventario() {
+        return inventario;
+    }
+
+    public void setInventario(List inventario) {
+        this.inventario = inventario;
+    }
+
+    public List getHabilidades() {
+        return habilidades;
+    }
+
+    public void setHabilidades(List habilidades) {
+        this.habilidades = habilidades;
+    }
+    
+   
+
+    
+   }   
+		
