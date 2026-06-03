e1091b6b745d96b7075e2cea2ffdd0a6187ccfe7
priaguero <prichiaguero@gmail.com>
2026-06-02 22:18:16 -0300

    subiendo

---
+++ src/main/java/dragonball/controller/ArmaService.java
@@ -0,0 +1,33 @@
+package dragonball.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import dragonball.IService.IArmaService;
+import dragonball.model.Arma;
+
+@Service
+public class  ArmaService implements IArmaService {
+    @Autowired
+    private ArmaRepository aRepository;
+    public List<Arma> findAllArmas() {
+        return aRepository.findAll();
+    }
+    public Arma saveArma(Arma arma) {
+        return aRepository.save(arma);
+    }
+
+    @Override
+    public List<Arma> getAllArmas() {
+        return aRepository.findAll();
+    }
+
+    @Override
+    public Arma createArma(Arma arma) {
+        return aRepository.save(arma);
+    }
+}
+
+interface ArmaRepository {
+    List<Arma> findAll();
+    Arma save(Arma arma);
+}
